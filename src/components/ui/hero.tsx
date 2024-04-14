64d4c5a77e3b68a4c0e059e63fad69b44871e49f
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-04-14 13:07:16 -0700

    Header

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 
 export default function Hero() {
-	return <div className={cn('bg-neutral-300 h-svh flex items-center justify-center')}>
-    <div className='h-24 bg-blue-700'></div>
-  </div>;
+	return (
+		<div
+			className={cn(
+				'bg-neutral-300 min-h-screen flex flex-col items-center justify-center'
+			)}>
+			<div className='w-[400px] bg-red-800 text-white text-center py-8 px-12 mb-8'>
+				<h2 className='text-4xl font-bold mb-4'>Kingdom of Light</h2>
+				<p className='text-2xl'>Coming Soon</p>
+			</div>
+			<div className='w-[400px]  bg-red-800 text-white text-center py-8 px-12'>
+				<h3 className='text-2xl font-bold mb-4'>Voice Actors</h3>
+				<p className='text-2xl'>WANTED</p>
+			</div>
+		</div>
+	);
 }
