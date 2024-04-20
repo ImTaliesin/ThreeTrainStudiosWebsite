1a7f879e4e344cd4479b6c994ea41801a5a85982
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-04-19 21:04:49 -0700

    Update About, Collaborate, and Donate page components with revised content and styling

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -7,36 +7,36 @@ export default function Donate() {
 			className={cn(
 				'bg-gradient-to-b from-sky-400 to-sky-200 min-h-screen flex items-center justify-center relative overflow-hidden'
 			)}>
-			<div className='bg-white p-8 rounded-lg shadow-lg w-96'>
+			<div className='container mx-auto px-4'>
+				<div className='max-w-lg mx-auto bg-neutral-950/70 rounded-lg shadow-lg p-8 text-white'>
+					<h2 className='text-2xl font-bold mb-4 text-center'>Why Help?</h2>
 
-				<h2 className='text-2xl font-bold mb-4 text-center'>Why Help?</h2>
+					<div className='mb-6'>
+						<h3 className='text-lg font-semibold mb-2 '>
+							Help Us Create More Free Content:
+						</h3>
+						<p className=''>
+							Each fund helps pay all of hard working creators to keep all of
+							our content free.
+						</p>
+					</div>
 
-				<div className='mb-6'>
-					<h3 className='text-lg font-semibold mb-2 '>
-						Help Us Create More Free Content:
-					</h3>
-					<p className='text-gray-700'>
-					Each fund helps pay all of hard working creators to keep all of our
-						content free.
-					</p>
-				</div>
+					<div className='mb-6'>
+						<h3 className='text-lg font-semibold mb-2'>
+							Special Features On The Show
+						</h3>
+						<p className=''>
+							Your fund can go towards having an appearance on the show, your
+							name in the credits, and more!
+						</p>
+					</div>
 
-				<div className='mb-6'>
-					<h3 className='text-lg font-semibold mb-2'>
-						Special Features On The Show
-					</h3>
-					<p className='text-gray-700'>
-						Your fund can go towards having an appearance on the show, your name
-						in the credits, and more!
-					</p>
+					<div className='flex justify-end'>
+						<Button className='bg-sky-500 text-white rounded hover:bg-sky-600 '>
+							Donate
+						</Button>
+					</div>
 				</div>
-				
-				<div className='flex justify-end'>
-					<Button className='bg-sky-500 text-white rounded hover:bg-sky-600 '>
-						Donate
-					</Button>
-				</div>
-        
 			</div>
 		</div>
 	);
